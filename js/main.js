ad75036be11feb66a353ef4f5d4117b2460f3b7a
ItzCarz <113217758+ItzCarz@users.noreply.github.com>
2026-02-09 12:31:59 -0500

    Inital Commit, Main Pages

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.tab-btn');
+const tabs = document.querySelectorAll('.tab');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('active'));
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    button.classList.add('active');
+    document.getElementById(button.dataset.tab).classList.add('active');
+  });
+});
